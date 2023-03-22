28001cd7385ac7e9c7b3734c5e09d4e11b3db666
tehi_kim <k.tehi21@gmail.com>
2023-03-22 11:30:08 +0000

    final solution

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ class Account {
 
   constructor(username) {
     this.username = username;
-    this.balance = 0;
+    this.transactions = [];
+  }
+
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for(let bal of this.transactions) {
+      balance += bal;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
@@ -15,7 +28,12 @@ class Transaction {
   }
 
   commit() {
-    this.account.balance += this.value;
+    if(!this.isAllowed()) return false;
+    // Keep track of the time of the transaction
+    this.time = new Date();
+    // Add the transaction to the account
+    this.account.addTransaction(this.value);
+    return true;
   }
 
 }
@@ -25,6 +43,11 @@ class Deposit extends Transaction {
   get value() {
     return this.amount
   }
+  isAllowed() {
+    // deposits always allowed thanks to capitalism.
+    return true;
+  }
+
 }
 
 class Withdrawal extends Transaction {
@@ -32,7 +55,10 @@ class Withdrawal extends Transaction {
   get value() {
     return -this.amount;
   }
-
+  isAllowed() {
+    // note how it has access to this.account b/c of parent
+    return (this.account.balance - this.amount >= 0);
+  }
 }
 
 
@@ -41,11 +67,21 @@ const myAccount = new Account('billybob');
 
 console.log('Starting Balance:', myAccount.balance);
 
-const t1 = new Deposit(120.00, myAccount);
-t1.commit();
+console.log('Attempting to withdraw even $1 should fail...');
+const t1 = new Withdrawal(1.00, myAccount);
+console.log('Commit result:', t1.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-const t2 = new Withdrawal(50.00, myAccount);
-t2.commit();
+console.log('Depositing should succeed...');
+const t2 = new Deposit(9.99, myAccount);
+console.log('Commit result:', t2.commit());
+console.log('Account Balance: ', myAccount.balance);
 
-console.log('Ending Balance:', myAccount.balance);
+console.log('Withdrawal for 9.99 should be allowed...');
+const t3 = new Withdrawal(9.99, myAccount);
+console.log('Commit result:', t3.commit());
 
+console.log('Ending Account Balance: ', myAccount.balance);
+console.log("Lookings like I'm broke again");
+
+console.log('Account Transaction History: ', myAccount.transactions);
